db9a5aed0082a3404c1b8fdd91aa116ff13b119f
PravinSelva5 <selvarap@mcmaster.ca>
2020-05-09 16:16:30 -0400

    Create Sphere_Volume.py

---
+++ Functions/Sphere_Volume.py
@@ -0,0 +1,12 @@
+'''
+Write a function that computes the volume of a sphere given its radius.
+'''
+
+
+import math
+def vol(rad):
+    volume = (4/3)*(math.pi)*(rad**3)
+    return volume
+
+# Check 1
+vol(2)
