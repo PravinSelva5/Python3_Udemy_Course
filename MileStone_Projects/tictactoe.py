d9d2241dd9b59533e7195e13081845351a3f496a
PravinSelva5 <selvarap@mcmaster.ca>
2020-05-24 20:52:10 -0400

    Update tictactoe.py

--- MileStone_Projects/tictactoe.py
+++ MileStone_Projects/tictactoe.py
@@ -1,7 +1,9 @@
 # Function prints outs the board
 def display_board(board):
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print("---------")
     print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print("---------")
     print(board[7] + ' | ' + board[8] + ' | ' + board[9])
 
 # Function takes in a player input and assigns their marker.
