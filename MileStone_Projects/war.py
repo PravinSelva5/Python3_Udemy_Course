d45ea18085aec01745f13de70c5bfc89b4e9a660
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-27 00:24:41 -0400

    updated war

---
+++ MileStone_Projects/war.py
@@ -0,0 +1,37 @@
+"""
+-----------
+CARD CLASS
+-----------
+- Needs to understand the suit of the card
+- Needs to understand the rank of the card
+- There needs to be an integer rank associated with the rank, so that you can compare two cards
+"""
+values = {
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6,
+    'Seven': 7,
+    'Eight': 8,
+    'Nine': 9,
+    'Ten': 10,
+    'Jack': 11,
+    'Queen': 12,
+    'King': 13,
+    'Ace': 14,
+}
+
+class Card():
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+
+
