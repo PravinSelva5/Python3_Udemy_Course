0765c55a3cafbc35448013c9a7e1bba224ade690
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-27 16:41:31 -0400

    completed war game

--- MileStone_Projects/war.py
+++ MileStone_Projects/war.py
@@ -48,3 +48,127 @@ class Deck():
         
     def shuffle_deck(self):
         random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+"""
+-------------
+PLAYER CLASS
+-------------
+- Class will be used to hold a player's current list of cards
+- Player should be able to ADD or REMOVE from their hand
+- Player should be able to add a single or multiple cards to their list
+"""
+
+class Player():
+
+    def __init__(self, name):
+        
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            # List for multiple card objects that need to be added to deck
+            self.all_cards.extend(new_cards)
+        else:
+            # List for adding a single card object
+            self.all_cards.append(new_cards)
+    
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards. '
+
+
+"""
+-------------
+GAME SETUP
+-------------
+"""
+
+player_one = Player('One')
+player_two = Player('Two')
+
+new_deck = Deck()
+new_deck.shuffle_deck()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+
+"""
+-------------
+GAME LOGIC
+-------------
+"""
+game_on = True
+round_number = 0
+
+while game_on:
+    round_number += 1
+    print(f'Round {round_number}')
+
+    # Check to see if someone has lost
+    if len(player_one.all_cards) == 0:
+        print('Player One, out of cards! Player Two Wins!')
+        game_on = False
+        break
+
+    if len(player_two.all_cards) == 0:
+        print('Player Two, out of cards! Player One Wins!')
+        game_on = False
+        break
+
+    # Start a new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+# -----------------------------------------
+#           While at war logic
+#------------------------------------------
+
+    at_war = True
+    
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            at_war = False
+        
+        else:
+            print("WAR!")
+
+            if len(player_one.all_cards) < 5:
+                print("Player One unable to declare war")
+                print("Player Two WINS!")
+                game_on = False 
+                break
+            
+            if len(player_two.all_cards) < 5:
+                print("Player Two unable to declare war")
+                print("Player One WINS!")
+                game_on = False 
+                break
+
+            else:
+                for num in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+    
