e6c3fb9ae28d857248996171f60be0e833a4aa6c
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-27 22:50:27 -0400

    completed black jack game

--- MileStone_Projects/blackjack.py
+++ MileStone_Projects/blackjack.py
@@ -26,8 +26,7 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
-             'Queen':10, 'King':10, 'Ace':11}
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 playing = True
 
@@ -47,7 +46,7 @@ class Deck:
         self.deck = []  # start with an empty list
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(suits,ranks))
+                self.deck.append(Card(suit,rank))
 
     def __str__(self):
         complete_deck = ''
@@ -72,7 +71,7 @@ class Hand:
     def add_card(self,card):
         #card from Deck.deal() --> single card (suit, rank)
         self.cards.append(card)
-        self.value += values[card.rank]
+        self.value += values[card.ranks]
         if card.ranks == 'Ace':
             self.aces += 1
 
@@ -102,9 +101,17 @@ Function for taking bets
 Since we're asking the user for an integer value, this would be a good place to use try,except.
 Remember to check that a Player's bet can be covered by their available chips.
 '''
-def take_bet():
-
-    pass
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet? "))
+        except:
+            print("Please provide an integer value.")
+        else:
+            if chips.bet > chips.total:
+                print("Sorry you don't have enough chips. You have: {}".format(chips.total))
+            else:
+                break
 
 '''
 Function for taking hits
@@ -114,7 +121,9 @@ You may want it to check for aces in the event that a player's hand exceeds 21.
 '''
 def hit(deck,hand):
 
-    pass
+    single_card = deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
 
 '''
 Function prompting the Player to Hit or Stand
@@ -123,9 +132,21 @@ If the Player Hits, employ the hit() function above. If the Player Stands, set t
 loop later on in our code.
 '''
 def hit_or_stand(deck,hand):
-    global playing  # to control an upcoming while loop
+    global playing
 
-    pass
+    while True:
+        x = input("Hit or Stand? Enter 'h' or 's': ")
+
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        
+        elif x[0].lower() == 's':
+            print("Player stands, Dealer's Turn")
+            playing = False
+        else:
+            print("Sorry i didn't understand that, please enter h or s only.")
+            continue
+        break
 
 '''
 Functions to display cards
@@ -133,72 +154,111 @@ When the game starts, and after each time Player takes a card, the dealer's firs
 At the end of the hand all cards are shown, and you may want to show each hand's total value. Write a function for each of these scenarios.
 '''
 def show_some(player,dealer):
-
-    pass
+    print('DEALERS HAND: ')
+    print('one card hidden!')
+    print(dealer.cards[1])
+    print('\n')
+    print('PLAYERS HAND:')
+    print('\n')
+    for card in player.cards:
+        print(card)
 
 def show_all(player,dealer):
-
-    pass
-
+    print('DEALERS HAND: ')
+    for card in dealer.cards:
+        print(card)
+    print('\n')
+    print('PLAYERS HAND: ')
+    print('\n')
+    for card in player.cards:
+        print(card)
 '''
 Functions to handle end of game scenarios. Remember to pass player's hand, dealer's hand and chips as needed.
 '''
-def player_busts():
-    pass
+def player_busts(player,dealer,chips):
+    print("BUST PLAYER \n")
+    chips.lose_bet()
 
-def player_wins():
-    pass
+def player_wins(player,dealer,chips):
+    print("PLAYER WINS!")
+    chips.win_bet()
 
-def dealer_busts():
-    pass
+def dealer_busts(player,dealer,chips):
+    print("PLAYER WINS! DEALER BUSTED!")
+    chips.win_bet()
 
-def dealer_wins():
-    pass
+def dealer_wins(player,dealer,chips):
+    print("DEALER WINS!")
+    chips.lose_bet()
 
-def push():
-    pass
+def push(player, dealer):
+    print("Player and Dealer tie! PUSH!")
 
 
 
 while True:
     # Print an opening statement
-
+    print("WELCOME TO BLACK JACK")
 
     # Create & shuffle the deck, deal two cards to each player
+    deck = Deck()
+    deck.shuffle()
 
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
 
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
 
     # Set up the Player's chips
-
+    player_chips = Chips()
 
     # Prompt the Player for their bet
-
+    take_bet(player_chips)
 
     # Show cards (but keep one dealer card hidden)
-
+    show_some(player_hand, dealer_hand)
 
     while playing:  # recall this variable from our hit_or_stand function
 
         # Prompt for Player to Hit or Stand
-
+        hit_or_stand(deck, player_hand)
 
         # Show cards (but keep one dealer card hidden)
-
+        show_some(player_hand, dealer_hand)
 
         # If player's hand exceeds 21, run player_busts() and break out of loop
-
-
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
             break
 
     # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
-
+    if player_hand.value <= 21:
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
 
         # Show all cards
-
+        show_all(player_hand, dealer_hand)
         # Run different winning scenarios
-
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
 
     # Inform Player of their chips total
-
+    print('Player total chips are at: {}'.format(player_chips.total))
     # Ask to play again
-    break
+    new_game = input("Would you like to play another hand? y/n: ")
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print("Thank you for playing!")
+        break
