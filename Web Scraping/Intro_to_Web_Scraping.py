94a827253aad49577d7cab127a3b2c79e0a003fb
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-28 22:48:26 -0400

    learning how to web scrap with python

---
+++ Web Scraping/Intro_to_Web_Scraping.py
@@ -0,0 +1,23 @@
+'''
+----------------------------------
+     RULES FOR WEB SCRAPING
+----------------------------------
+
+- Always try to get permission before scraping, 
+- If you don't and make too many attempts/requests, your IP maybe blocked
+
+
+----------------------------------
+    LIMITATIONS OF WEB SCRAPING
+----------------------------------
+
+- Every website it unique, therefore every web scraping script is UNIQUE
+- A slight change or update to a website MAY completely break your web scraping script
+
+
+AT COMMAND LINE:    
+
+pip3 install requests  -- allows you to make a request to a website and get information from it
+pip3 install lxml -- allows you to decipher whatever requests returns
+pip install bs4 
+'''
