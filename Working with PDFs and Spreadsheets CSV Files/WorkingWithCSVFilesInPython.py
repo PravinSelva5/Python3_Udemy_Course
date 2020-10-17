a2ebfd51c86f1025995fcdd8f84718fdb75bc347
PravinSelva5 <selvarap@mcmaster.ca>
2020-10-17 00:48:53 -0400

    working with csv and pdfs

---
+++ Working with PDFs and Spreadsheets CSV Files/WorkingWithCSVFilesInPython.py
@@ -0,0 +1,19 @@
+'''
+Libraries to consider when working with CSV files:
+   - built-in csv module in Python
+   - Pandas
+        - Full data analysis library, can work with almost any tabular data type.
+        - Runs visualizations and analysis
+   - Openpyxl
+        - specifically designed for excel files
+        - retains a lot of Excel specific functionality
+        - Supports Excel formulas
+   - Google Sheets Python API
+        - allows you to directly make changes to the spreadsheets hosted online
+'''
+
+import csv 
+
+# Open File
+
+# csv.reader
