eebf0c414538e3550673d3e470dfc05becc3a458
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-01-05 20:06:41 -0500

    went through PDF and spreadsheet module

--- Working with PDFs and Spreadsheets CSV Files/PDF_Spreadsheets_PythonPuzzle.py
+++ Working with PDFs and Spreadsheets CSV Files/PDF_Spreadsheets_PythonPuzzle.py
@@ -1,7 +1,9 @@
 import csv
 import PyPDF2
 
-# Task 1
+###########
+# Task 1 #
+##########
 
 data = open('/Users/PravinSelvarajah/Documents/GitHub/Python_Udemy_Bootcamp/Working with PDFs and Spreadsheets CSV Files/Exercise_Files/find_the_link.csv')
 
@@ -9,5 +11,39 @@ csv_data = csv.reader(data)
 
 # Reformat the csv file as a list of lists
 data_lines = list(csv_data)
+link = ''
 
-print(data_lines)
+
+for num in range(len(data_lines)):
+    link += data_lines[num][num]
+
+print(link)
+
+##########
+# Task 2 #
+##########
+
+f = open('Exercise_Files/Find_the_Phone_Number.pdf','rb')
+
+pdf_read = PyPDF2.PdfFileReader(f)
+
+# Number of Pages
+print(pdf_read.numPages)
+
+import re
+
+pattern = r'\d{3}.\d{3}.\d{4}'
+
+all_text = ' '
+
+for n in range(pdf_read.numPages):
+
+    page = pdf_read.getPage(n)
+    page_text = page.extractText()
+
+    all_text = all_text + ' ' + page_text
+
+for match in re.finditer(pattern,all_text ):
+    print(match)
+
+# all_text[41809:41808+20]   Looking at the pattern, regualr expression can be adjusted
